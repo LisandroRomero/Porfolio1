fc4e7df059b5102e50ba917b44b75afcdb6b3158
LisanRomero <lisanromero18@gmail.com>
2023-07-07 11:52:02 -0300

    contact page added (for now)

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import './App.css'
 import { Route, Routes } from 'react-router-dom';
 import Landing from './View/Landing/Landing';
 import NavBar from './Components/NavBar/NavBar';
+import Contact from './Components/Contact/Contact';
 function App() {
 
 
@@ -11,7 +12,7 @@ function App() {
       <NavBar/>
     <Routes>
       <Route path={"/"} Component={Landing} />
-      
+      <Route path={"/contact"} Component={Contact} />
     </Routes>
     </div>
   )
