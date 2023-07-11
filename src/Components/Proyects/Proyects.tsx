32a9c7e5769e68bdca592a236ae4a28866af090b
LisanRomero <lisanromero18@gmail.com>
2023-07-11 13:57:26 -0300

    proyects part added

---
+++ src/Components/Proyects/Proyects.tsx
@@ -0,0 +1,21 @@
+
+const Proyects = () => {
+    return (
+        <div>
+            <div>
+                <h3>Airland</h3>
+                <p>E-commerce de zapatiilas Nike Air Force realizado con otras 6 personas /n
+
+                </p>
+
+            </div>
+
+            <div>
+                <h3>PokeApp</h3>
+                <p>SPA(Single page aplication) consumiendo una API de pokemon</p>
+            </div>
+        </div>
+    );
+};
+
+export default Proyects;
